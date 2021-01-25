00d0a5f11c341200985cc2c88d3c8fc5cc3c2a68
Rodolfo <rodolfoafl@gmail.com>
2021-01-25 14:55:50 -0300

    Finalized project 27

---
+++ toast_notification/script.js
@@ -0,0 +1,30 @@
+const button = document.getElementById("button");
+const toasts = document.getElementById("toasts");
+
+const messages = [
+  "Message One",
+  "Message Two",
+  "Message Three",
+  "Message Four",
+];
+
+button.addEventListener("click", () => {
+  createNotification();
+});
+
+function createNotification() {
+  const notification = document.createElement("div");
+  notification.classList.add("toast");
+
+  notification.innerText = getRandomMessage();
+
+  toasts.appendChild(notification);
+
+  setTimeout(() => {
+    notification.remove();
+  }, 3000);
+}
+
+function getRandomMessage() {
+  return messages[Math.floor(Math.random() * messages.length)];
+}
